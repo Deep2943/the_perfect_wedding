a14b8de2f121b8dd82cb8d7f2b002f2cb7401849
Deep Suthar <suthardeep2624@gmail.com>
2023-04-09 22:47:52 +0530

    Final Header Section

--- header.php
+++ header.php
@@ -17,7 +17,6 @@ require("connection.php");
 	<![endif]-->
 
     <!-- <link rel="stylesheet" href="css/bootstrap.css" type="text/css"> -->
-    <link rel="stylesheet" href="css/new-style.css" type="text/css">
     <link rel="stylesheet" href="css/bootstrap/css/bootstrap.css" type="text/css">
     <link rel="stylesheet" href="css/bootstrap/css/bootstrap.min.css" type="text/css">
     <link rel="stylesheet" href="css/animate.css" type="text/css">
@@ -29,6 +28,8 @@ require("connection.php");
     <!-- <link rel="stylesheet" href="css/style - backup.css" type="text/css"> -->
     <link rel="stylesheet" href="css/style.css" type="text/css">
     <link rel="stylesheet" href="js/aos/aos.css" type="text/css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+    <link rel="stylesheet" href="css/new-style.css" type="text/css">
 
     <!-- custom background -->
     <link rel="stylesheet" href="css/bg.css" type="text/css">
@@ -37,7 +38,6 @@ require("connection.php");
     <link rel="stylesheet" href="css/color.css" type="text/css" id="colors">
     <script src="css/bootstrap/js/bootstrap.js"></script>
     <script src="js/aos/aos.js"></script>
-    <script src="css/bootstrap/js/bootstrap.min.js"></script>
     <script src="css/bootstrap/js/bootstrap.bundle.js"></script>
     <script src="js/script.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
@@ -45,32 +45,37 @@ require("connection.php");
 
 <body>
     <div class="nav-contact">
-        <div class="container d-flex justify-content-between align-items-center  p-1">
-            <a href="javascript:void(0)">Demo1</a>
-            <a href="javascript:void(0)">Demo2</a>
+        <div class="container d-flex justify-content-between align-items-center p-1">
+            <div class="d-flex align-items-center order-2">
+                <i class="fa-solid fa-envelope top-icon"></i><a class="icon-text me-3" href="mailto:demo@gmail.com">demo@gmail.com</a>
+                <i class="fa-solid fa-phone top-icon"></i><a class="icon-text me-3" href="tel:9898989898">+91 98989
+                    89898</a>
+                <i class="fa-solid fa-location-dot top-icon"></i><a class="icon-text" href="">Ahmedabad, India</a>
+            </div>
+            <a href="javascript:void(0)"></a>
         </div>
     </div>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a class="navbar-brand bottom-header-text" href="/the_perfect_wedding">The Perfect Wedding</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
                 aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
-                <ul class="navbar-nav">
+                <ul class="navbar-nav" id="nav">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="/the_perfect_wedding">Home</a>
+                        <a class="nav-link" aria-current="page" id="home-link" href="/the_perfect_wedding">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="aboutus.php">About Us</a>
+                        <a class="nav-link" href="aboutus.php" id="about-link">About Us</a>
                     </li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                            aria-expanded="false">
+                            aria-expanded="false" id="service-link">
                             Services
                         </a>
-                        <ul class="dropdown-menu">
+                        <ul class="dropdown-menu header-dropdown">
                             <li><a class="dropdown-item" href="venues.php">Venues</a></li>
                             <li><a class="dropdown-item" href="decoration.php">Decoration</a></li>
                             <li><a class="dropdown-item" href="catering.php">Catering</a></li>
@@ -78,25 +83,27 @@ require("connection.php");
                         </ul>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="gallery.php">Gallery</a>
+                        <a class="nav-link" href="gallery.php" id="gallery-link">Gallery</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="profile.php">Profile</a>
+                        <a class="nav-link" href="profile.php" id="profile-link">Profile</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="contactus.php">Contact Us</a>
+                        <a class="nav-link" href="contactus.php" id="contact-link">Contact Us</a>
                     </li>
                     <?php 
             if(isset($_SESSION['email_id']))
             {
         ?>
-                    <li class="nav-item"><a class="nav-link" href="logout.php">Logout</a></li>
+                    <a class="login-out-btn" href="logout.php"><i
+                            class="fa-solid fa-right-from-bracket login-out-icon"></i> Logout</a>
                     <?php    
             }
             else
             {
         ?>
-                    <li class="nav-item"><a href="login1.php" class="nav-link">Login</a></li>
+                    <a href="login1.php" class="login-out-btn"><i
+                            class="fa-solid fa-right-to-bracket login-out-icon"></i> Login</a>
                     <?php
             }
         ?>
@@ -104,46 +111,4 @@ require("connection.php");
             </div>
         </div>
     </nav>
-    <?php /*  <div id="wrapper">
-        <header class="bottom-header">
-            <div class="container bottom-header-content">
-                <!-- <div id="logo">
-                    <a href="index.php">
-                        <h2 class="bottom-header-text">The Perfect Wedding</h2>
-                    </a>
-                </div> -->
-                <nav>
-                    <ul id="mainmenu">
-                        <li><a href="index.php">Home</a></li>
-                        <li><a href="gallery.php">Gallery</a></li>
-                        <li><a href="javascript:void(0)">Services</a>
-                            <ul>
-                                <li><a href="venues.php">Venues</a></li>
-                                <li><a href="decoration.php">Decoration</a></li>
-                                <li><a href="catering.php">Catering</a></li>
-                                <li><a href="sound.php">DJ/Sound</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="contactus.php">Contact Us</a></li>
-                        <li><a href="aboutus.php">About Us</a></li>
-                        <li><a href="profile.php">Profile</a></li>
-                        <?php 
-                            if(isset($_SESSION['email_id']))
-                            {
-                        ?>
-    <li><a href="logout.php">Logout</a></li>
-    <?php    
-                            }
-                            else
-                            {
-                        ?>
-    <li><a href="login1.php">Login</a></li>
-    <?php
-                            }
-                        ?>
-    </ul>
-    </nav>
-    </div>
-    </header>
-    </div> */ ?>
     <!-- header close -->
